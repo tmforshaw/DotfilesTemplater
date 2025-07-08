76c214dd8c3bd35fc66345ac2d1623b2da722477
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2025-07-08 14:18:20 +0100

    std::fmt::Display not necessary, just use pretty print

--- src/config.rs
+++ src/config.rs
@@ -1,72 +1,21 @@
-use std::fmt;
-
 use lazy_static::lazy_static;
 use serde::Deserialize;
 
 use crate::file::open_file;
-// use toml::Table;
 
 static CONFIG_PATH: &str = "/home/tmforshaw/.config/dotfile-templater/config.toml";
 pub const FUNCTION_CHAR: char = '@';
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    pub background_colour: String,
-    pub files: FileConfigs,
-}
-
-impl fmt::Display for Config {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "Config {{
-    background_colour: {:?},
-    files: {:?}
-}}",
-            self.background_colour, self.files
-        )
-    }
+    pub _background_colour: String,
+    pub files: Vec<FileConfig>,
 }
 
 #[derive(Deserialize, Debug, Clone)]
 pub struct FileConfig {
     pub file: String,
     pub comment_char: char,
-}
-
-impl fmt::Display for FileConfig {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{{file: {:?}, comment_char: {:?}}}",
-            self.file, self.comment_char
-        )
-    }
-}
-
-#[derive(Deserialize, Debug, Clone)]
-pub struct FileConfigs {
-    pub configs: Vec<FileConfig>,
-}
-
-impl fmt::Display for FileConfigs {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut message = String::new();
-        message += "[";
-
-        for file_config in self.configs.iter() {
-            message += format!("{file_config}").as_str();
-        }
-        message += "]";
-
-        write!(f, "{message}")
-    }
-}
-
-impl From<FileConfigs> for Vec<FileConfig> {
-    fn from(value: FileConfigs) -> Self {
-        value.configs
-    }
 }
 
 lazy_static! {
@@ -77,7 +26,7 @@ lazy_static! {
 fn parse_config() -> Config {
     let config = toml::from_str(open_file(CONFIG_PATH).as_str()).unwrap();
 
-    println!("{config}");
+    println!("{config:#?}");
 
     config
 }
